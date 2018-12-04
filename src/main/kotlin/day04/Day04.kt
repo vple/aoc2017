cc953034890268e3a351d8291ca8463a282db2ec
Vincent Le <vple@yext.com>
2018-12-03 23:14:16 -0500

    Day 4 solutions.

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -10,8 +10,30 @@ const val INPUT_FILE = "/day04/input.txt"
 
 fun main(args: Array<String>) {
     val input = loadResource(INPUT_FILE)
+    val passphrases = input.lines().filterNot { it.isEmpty() }
 
     println("Part 1")
+    println(countValidPassphrases(passphrases) { it })
 
     println("Part 2")
+    println(countValidPassphrases(passphrases) { it.groupingBy { it }.eachCount() })
 }
+
+/**
+ * Splits the given [passphrase] into words.
+ */
+fun passphraseWords(passphrase: String) = passphrase.split(" ")
+
+/**
+ * Returns true if the given [passphrase] is valid.
+ */
+fun <T> isValidPassphrase(passphrase: String, hash: (String) -> T): Boolean {
+    val words = passphraseWords(passphrase)
+    return words.count() == words.map(hash).toSet().count()
+}
+
+/**
+ * Returns the number of [passphrases] that are valid.
+ */
+fun <T> countValidPassphrases(passphrases: List<String>, hash: (String) -> T) =
+    passphrases.filter { isValidPassphrase(it, hash) }.count()
